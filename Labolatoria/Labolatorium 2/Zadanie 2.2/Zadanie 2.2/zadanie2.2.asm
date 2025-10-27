56adc5ab7a2e214dcc19b06cbcda2185879b0bc5
Jacek Urbanowicz <jacek.urbanowicz2@gmail.com>
2025-10-27 03:24:37 +0100

    Dodanie zadania 2.2

---
+++ Labolatoria/Labolatorium 2/Zadanie 2.2/Zadanie 2.2/zadanie2.2.asm
@@ -0,0 +1,41 @@
+; Przyk³ad wywo³ywania funkcji MessageBoxA i MessageBoxW 
+.686 
+.model flat 
+extern     _ExitProcess@4  : PROC 
+extern     _MessageBoxA@16 : PROC 
+extern     _MessageBoxW@16 : PROC 
+public     _main 
+
+.data 
+tytul_Unicode dw   'T','e','k','s','t',' ','w',' ' 
+			  dw   'f','o','r','m','a','c','i','e',' ' 
+			  dw   'U','T','F','-','1','6', 0
+
+tekst_Unicode dw 'K','a', 017Ch ,'d','y',' ','z','n','a','k',' ' ; Zmiana wyœwietlania z na ¿
+			  dw 'z','a','j','m','u','j','e',' ' 
+			  dw '1','6',' ','b','i','t', 00F3h ,'w', 0 ; Zmiana wyœwietlania o na ó
+
+tytul_Win1250 db 'Tekst w standardzie Windows 1250', 0 
+
+tekst_Win1250 db 'Ka', 0BFh ,'dy znak zajmuje 8 bit', 0F3h ,'w', 0 ; Zmiana wyœwietlania z na ¿ i o na ó
+
+.code 
+_main PROC 
+	push    0       ; sta³a MB_OK 
+	; adres obszaru zawieraj¹cego tytu³ 
+	push    OFFSET tytul_Win1250  
+	; adres obszaru zawieraj¹cego tekst 
+	push    OFFSET tekst_Win1250 
+	push    0       ; NULL 
+	call    _MessageBoxA@16 
+	push    0       ; stala MB_OK 
+	; adres obszaru zawieraj¹cego tytu³ 
+	push    OFFSET tytul_Unicode 
+	; adres obszaru zawieraj¹cego tekst 
+	push    OFFSET tekst_Unicode 
+	push    0       ; NULL 
+	call    _MessageBoxW@16 
+	push    0       ; kod powrotu programu 
+	call    _ExitProcess@4 
+_main ENDP
+END 
